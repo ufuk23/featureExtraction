d2388733877bef939f9f2fff67a25a7be3ed052d
ufuk23 <ufuktul@gmail.com>
2022-07-22 11:59:51 +0300

    kam services

---
+++ milvus2_kam_create_vectors.py
@@ -0,0 +1,178 @@
+import pymssql
+from tensorflow.keras.preprocessing import image
+import requests
+import numpy as np
+from scipy import spatial
+from sklearn.preprocessing import minmax_scale
+import json
+import time
+import logging
+from pymilvus import (
+    connections,
+    utility,
+    FieldSchema, CollectionSchema, DataType,
+    Collection,
+)
+
+
+# Gets or creates a logger
+logger = logging.getLogger(__name__)
+# set log level
+logger.setLevel(logging.DEBUG)
+# define file handler and set formatter
+file_handler = logging.FileHandler('log_KAM_create_vector.log')
+formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+file_handler.setFormatter(formatter)
+# add file handler to logger
+logger.addHandler(file_handler)
+
+conn = None
+GAP = 10  # seconds to sleep between the loop steps
+
+# handle milvus collection
+print("start connecting to Milvus")
+connections.connect("default", host="localhost", port="19530")
+has = utility.has_collection("artifact")
+print(f"Does collection artifact exist in Milvus: {has}")
+
+fields = [
+    FieldSchema(name="pk", dtype=DataType.INT64, is_primary=True, auto_id=False),
+    FieldSchema(name="artifact_type", dtype=DataType.INT64),
+    FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=2048)
+]
+
+# description as "artifact" below is mandatory to connect
+schema = CollectionSchema(fields, "artifact")
+print("Collection artifact")
+artifact = Collection("artifact", schema, consistency_level="Strong")
+
+
+# Model REST API - tf serving - predict service URL
+# tf_serving_url = 'http://localhost:8501/v1/models/similarityModel:predict'
+tf_serving_url = 'http://localhost:8501/v1/models/resnet50:predict'
+headers = {"content-type": "application/json"}
+
+# mount path to access the file Server
+# fs = "/mnt/muesfs/mues-images/image/ak/" # prod
+fs = "/mnt/muesfs/mues/mueskam-images/dev/image/ak/" # dev
+fs_mues = "/mnt/muesfs/mues/mues-images/dev/image/ak/"
+
+# MILVUS REST API URL
+#milvus_url = 'http://localhost:19121/collections/artifact/vectors'
+#milvus_url = 'http://localhost:19121/collections/kam/vectors'
+
+
+def prepare_image(img, target_size=(224,224)):
+    img = img.resize(target_size)
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+
+    img = image.img_to_array(img)
+    # img = np.array(img)
+    img = np.expand_dims(img, axis=0)
+    # img = preprocess_input(img)
+    return img
+
+
+def connect_to_db():
+    global conn
+    try:
+        conn = pymssql.connect(server='10.1.37.177', port='1033', user='muest', password='Mues*test.1', database='mues_test')
+        logger.info('DB connected successfully')
+    except Exception as e:
+        logger.critical(e)
+
+
+def create_top_n_vectors():
+    cursor = conn.cursor()
+    cursor.execute("select DISTINCT TOP 100 K.uid, F.FOTOGRAF_PATH, F.artifactId from KAM_ARTIFACT_VIEW K "
+                   "LEFT JOIN Kam_ArtifactPhotograph F ON K.artifactId = F.artifactId "
+                   "WHERE K.artifactType!='INVENTORY_ARTIFACT' AND K.aktif=1 AND K.silinmis=0 AND F.ANA_FOTOGRAF=1 AND F.FOTOGRAF_PATH is not null AND F.FEATURE_VECTOR_STATE is NULL ORDER BY K.uid")
+
+    records = cursor.fetchall()
+
+    ids = []
+    vectors = []
+    artifact_types = []
+    ok_list = []
+    err_list = []
+
+    for row in records:
+        try:
+            logger.info("uid:" + str(row[0]) + " : " + str(row[1]))
+            print(("uid: " + str(row[0]) + " : " + str(row[1])))
+
+            img = image.load_img(fs + row[1])
+            img_data = prepare_image(img)
+
+            # prepare for tf serving service
+            # give the photo and get the vector from the model
+            data = json.dumps({"signature_name": "serving_default", "instances": img_data.tolist()})
+            response = requests.post(tf_serving_url, data=data, headers=headers)
+            dict_resp = json.loads(response.text)
+            feature_np = np.array(dict_resp["predictions"])
+            # min-max scale the data between 0 and 1
+            scaled_vec = minmax_scale(feature_np.flatten())
+            result_vec = np.round(scaled_vec, 2)
+            # print(result_vec)
+
+            # for milvus request
+            ids.append(row[0])
+            # KAM artifact_type = 2, MUES artifact_type = 1
+            artifact_types.append(2)
+            # print(artifact_types)
+            vectors.append(result_vec.tolist())
+
+            ok_list.append(str(row[2]))
+
+        except (FileNotFoundError, IOError):
+            logger.error("File not found: " + fs + row[1])
+            err_list.append(str(row[0]))  # marking for FileNotFound
+        except ValueError as e:
+            logger.error("Decoding JSON has failed")
+            logger.error(e)
+        except (requests.HTTPError, requests.RequestException) as e:
+            logger.error("HTTP/Request error occurred")
+            logger.error(e)
+
+    try:
+        # save the n vector to the Milvus DB
+        if(len(vectors)):
+            entities = [ids, artifact_types, vectors]
+            insert_result = artifact.insert(entities)
+    except Exception as e:
+        logger.error("MILVUS post request error (KAM)")
+        logger.error(e)
+
+    try:
+        # commit for top N selected records
+        if(len(ok_list)>0):
+            cursor.execute("UPDATE Kam_ArtifactPhotograph set FEATURE_VECTOR_STATE='1' where ANA_FOTOGRAF=1 AND artifactId in {}".format(str(tuple(ok_list)).replace(',)', ')')))
+        if(len(err_list)>0):
+            cursor.execute("UPDATE Kam_ArtifactPhotograph set FEATURE_VECTOR_STATE='-1' where ANA_FOTOGRAF=1 AND artifactId in {}".format(str(tuple(err_list)).replace(',)', ')')))
+
+        conn.commit()
+
+    except Exception as e:
+        logger.error(e)
+        logger.info("Trying to reconnect to the DB...")
+        conn.close()
+        connect_to_db()
+
+    return len(records)
+
+
+def create_all():
+    while True:
+        records_len = create_top_n_vectors()
+        print(str(records_len) + " KAM_vectors created successfully")
+        logger.info(str(records_len) + " KAM_vectors created successfully")
+        time.sleep(GAP)
+        # if records_len == 0:
+            # logger.info("No record found to get the KAM vector")
+            # break
+
+
+if __name__ == "__main__":
+    connect_to_db()
+    create_all()
---
+++ milvus2_kam_vectorSearchServer.py
@@ -0,0 +1,149 @@
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from sklearn.preprocessing import minmax_scale
+from PIL import Image
+import flask
+import io
+import os
+import json
+import requests
+import logging
+from pymilvus import (
+    connections,
+    utility,
+    FieldSchema, CollectionSchema, DataType,
+    Collection,
+)
+
+app = flask.Flask(__name__)
+#UPLOAD_FOLDER = "/home/muesd/similarity/uploads"
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# logger
+app.logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('flask.log')
+formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+file_handler.setFormatter(formatter)
+app.logger.addHandler(file_handler)
+
+
+
+# handle milvus collection
+print("start connecting to Milvus")
+connections.connect("default", host="localhost", port="19530")
+has = utility.has_collection("art")
+print(f"Does collection art exist in Milvus: {has}")
+
+fields = [
+    FieldSchema(name="pk", dtype=DataType.INT64, is_primary=True, auto_id=False),
+    FieldSchema(name="artifact_type", dtype=DataType.INT64),
+    FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=2048)
+]
+
+schema = CollectionSchema(fields, "art is the simplest demo to introduce the APIs")
+print("Collection art")
+art = Collection("art", schema, consistency_level="Strong")
+print("Start loading Milvus collection")
+art.load()
+
+search_params = {
+    "metric_type": "l2",
+    "params": {"nprobe": 16},
+}
+
+
+
+# Model REST API - tf serving - predict service URL
+# tf_serving_url = 'http://172.17.0.4:8501/v1/models/resnet50:predict'
+tf_serving_url = 'http://localhost:8501/v1/models/resnet50:predict'
+
+headers = {"content-type": "application/json"}
+
+# MILVUS REST API URL
+# milvus_search_url = 'http://172.17.0.3:19121/collections/kam/vectors'
+milvus_search_url = 'http://localhost:19121/collections/kam/vectors'
+
+
+def prepare_image(img, target_size=(224,224)):
+    img = img.resize(target_size)
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    # img = preprocess_input(img)
+    return img
+
+
+# gets the feature vector from tf serving model and search it on Milvus DB
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        test_img = None
+        vectors = []
+
+        app.logger.info("prediction service has called")
+
+        if flask.request.method == "POST":
+            if flask.request.files.get("image"):
+
+                #file = flask.request.files["image"]
+                #path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                #file.save(path)
+
+                # TODO: load_img de resize olmasiyla olmamasi arasinda sonuc vektor fark ediyor
+                # test_img = image.load_img(path, target_size=(224,224))
+                #test_img = image.load_img(path) #  bu sekilde PIL imdage load ile ayni oluyor
+                print("POST req is done")
+                test_img = flask.request.files["image"].read()
+                test_img = Image.open(io.BytesIO(test_img))
+                # fileName = flask.request.args.get("filename")
+
+        req_artifact_type = flask.request.form.get("artifact_type")
+        print("artifactType: ", req_artifact_type)
+        req_ids = flask.request.form.get("ids")
+        print("img: ", test_img)
+        img_data = prepare_image(test_img)
+
+        # prepare for tf serving service
+        # give the photo and get the vector from the model
+        data = json.dumps({"signature_name": "serving_default", "instances": img_data.tolist()})
+        response = requests.post(tf_serving_url, data=data, headers=headers)
+        dict_resp = json.loads(response.text)
+        feature_np = np.array(dict_resp["predictions"])
+        # min-max scale the data between 0 and 1
+        scaled_vec = minmax_scale(feature_np.flatten())
+        result_vec = np.round(scaled_vec, 2)
+        vectors.append(result_vec.tolist())
+        print(result_vec)
+
+        # milvus 2.x
+        req_expr = "pk in [" + req_ids.strip() +  "]" if req_ids != None else ""
+        req_expr = req_expr + (" and " if req_ids != None and req_artifact_type != None else "")
+        req_expr = req_expr + (("artifact_type == " + req_artifact_type) if req_artifact_type != None else "")
+
+        resp_milvus = art.search(vectors, "embeddings", search_params, limit=100, expr=req_expr, output_fields=["artifact_type"])
+        print("req_exp: ", req_expr)
+        print("MILVUS RESPONSE:", resp_milvus)
+        json_data=[]
+
+        for hits in resp_milvus:
+            for h in hits:
+                json_data.append({'id': h.id, 'distance':h.distance, 'artifact_type':h.entity.get('artifact_type')})
+                # print(f"hit: {h}, artifact_type field: {h.entity.get('artifact_type')}")
+
+        print(json.dumps(json_data))
+        return json.dumps(json_data)
+
+    except ValueError as e:
+        app.logger.error("Decoding JSON has failed")
+        app.logger.error(e)
+    except (requests.HTTPError, requests.RequestException) as e:
+        app.logger.error("HTTP/Request error occurred")
+        app.logger.error(e)
+
+    return "{'error': 'Unexpected Error'}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001)
